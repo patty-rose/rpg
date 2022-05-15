023113b38d5fc9950289086a8c74d45247809003
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-05-15 11:00:11 -0700

    add equip test
    co-authored-by: Mauro Rosales <mauro.rosales247@gmail.com>;

--- _tests_/game.test.js
+++ _tests_/game.test.js
@@ -14,4 +14,10 @@ describe('Player', () => {
     expect(player.hp).toEqual(100);
     expect(player.level).toEqual(1);
   })
+
+  test('should equip player with the correct weapon based on player type', () => {
+    const player = new Player("scientist");
+    player.equip();
+    expect(player.weapon).toEqual("chemical burn");
+  })
 })
--- src/js/game.js
+++ src/js/game.js
@@ -13,3 +13,4 @@ export class Player{
     this.level = 1;
   }
 }
+
